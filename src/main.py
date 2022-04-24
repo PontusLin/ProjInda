d00967bcd38cdc20b6481f025629fbf147eefd26
github-private <pontus.linde@me.com>
2022-04-24 12:49:21 +0200

    added ability to move

--- src/main.py
+++ src/main.py
@@ -7,26 +7,18 @@ from tank import tank
 # variables
 (width, height) = (1200, 800)
 
-
-up = (0, 1)
-down = (0, -1)
-right = (1, 0)
-left = (-1, 0)
-
 # main class
 def main():
     # initialize pygame
     pygame.init()
     
     # create basic screen
-    
     font = pygame.font.Font(None, 40)
-    
     screen = pygame.display.set_mode((width, height))
-    
     surf_background = pygame.image.load('assets/background.png').convert_alpha()
+    pygame.display.set_caption('Tanks')
     
-    #surf_welcome = font.render('Welcome to TANKS!',False, 'Green')
+    playerTank = tank(75, 535, screen, 'assets/playertank.png')
     
     surf_playertank = pygame.image.load('assets/playertank.png').convert_alpha()
     rect_playertank = surf_playertank.get_rect(midbottom = (75, 535))
@@ -37,32 +29,37 @@ def main():
     surf_obstacle = pygame.image.load('assets/obstacle.png').convert_alpha()
     rect_obstacle1 = surf_obstacle.get_rect(topleft = (100, 1))
     
-    pygame.display.set_caption('Tanks')
+    
     
     
     # create clock object to ensure good fps
     clock = pygame.time.Clock()
     
     running = True
+    # main game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.QUIT()
                 exit()
-            keys = pygame.key.get_pressed()
+       
+       # Handle key input
+        keys = pygame.key.get_pressed()
 
-            if keys[pygame.K_LEFT]:
-                pass
-            if keys[pygame.K_RIGHT]:
-                pass
-            if keys[pygame.K_UP]:
-                pass
-            if keys[pygame.K_DOWN]:
-                pass
+        # Check if any keys are pressed
+        if keys[pygame.K_LEFT]:
+            playerTank.move(-3, 0)
+        if keys[pygame.K_RIGHT]:
+            playerTank.move(3, 0)
+        if keys[pygame.K_UP]:
+            playerTank.move(0, -3)
+        if keys[pygame.K_DOWN]:
+            playerTank.move(0, 3)
+        
         # draw screen 
         screen.blit(surf_background, (0,0))
-        #screen.blit(surf_welcome, (100, 50))
-        screen.blit(surf_playertank, rect_playertank)
+        playerTank.blit()
+        #screen.blit(surf_playertank, rect_playertank)
         screen.blit(surf_enemytank, rect_enemytank)
         screen.blit(surf_obstacle, rect_obstacle1)
         
--- src/tank.py
+++ src/tank.py
@@ -1,23 +1,32 @@
-# a class for the tank in pygame
+import pygame
+#a class for the tank in pygame
 
 class tank:
-    
-    def __init__(self, xpos, ypos):
-        (xPos, yPos) = (xpos, ypos)
-        velocity = 0
-        health = 3
+    def __init__(self, xpos, ypos, screen, assetPicture):
+        self.screen = screen
+        self.xPos = xpos
+        self.yPos = ypos 
+        self.velocity = 0
+        self.health = 3
+        self.surf_tank = pygame.image.load(assetPicture).convert_alpha()
+        self.rect_tank = self.surf_tank.get_rect(midbottom = (self.xPos, self.yPos))
         
-    def move(self, direction):
-        pass
+    def move(self, directionX, directionY):
+        self.rect_tank.x += directionX
+        self.rect_tank.y += directionY
     
     def decrementHealth(init):
+        health -= 1;
+        if health < 1:
+            pass # Game over, You won/lose ... blablabla
+    
+    def blit(self):
+        # draw the tank to the screen
+        self.screen.blit(self.surf_tank, self.rect_tank)
+
+    def shoot(self):
         pass
     
-    def draw(self):
-        pass
-    
-    def handle_keys(self):
-        pass
     
     
 
